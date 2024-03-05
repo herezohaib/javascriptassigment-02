5bf06b3643f9c96ac6aa03a6ddedaef8e6ecbe2a
herezohaib <154750463+herezohaib@users.noreply.github.com>
2024-03-05 21:26:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+var username = "Zohaib";
+
+alert(username);
+
+var myname = "zohaib Ali";
+alert(myname);
+
+var fullname = 'Zohaib Ali';
+alert(fullname);
+
+var num = "are you sure you want to delete?";
+alert(num);
+
+var hy = "hello world";
+alert(hy);
+
+alert("messages");
+
+var biodata = "Certified mobile Application Development";
+alert(biodata);
+
+var pizza = "PIZZA\nPIZZ\nPLZ\nPI\nP"
+alert(pizza);
+
+var email = "my email address is za3924322@gmail.com";
+alert(email);
+ var book = "I am trying to learn from the book A smarter way to learn Javascript";
+ alert(book);   
+
+ document.getElementById("demo").innerHTML = 5 + 6;
+
+
+
+
+
+
+
+
+
+
+
+
